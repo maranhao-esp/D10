1608abb8719d76de940a763324dfeb77ab42123d
maranhao-esp <caua60198@gmail.com>
2026-04-18 08:32:19 -0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,27 @@
+const CACHE = 'nodois-v1';
+const ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html'))));
+});
+
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : { title: 'NósDois 💕', body: 'Nova mensagem do seu amor!' };
+  e.waitUntil(self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/icon-192.png',
+    badge: '/icon-192.png',
+    vibrate: [200, 100, 200],
+    tag: 'nodois-message'
+  }));
+});
